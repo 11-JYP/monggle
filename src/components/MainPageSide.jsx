81ec9e2e562739cf9c98e4a215b78adf18ecfc48
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-09-17 23:48:52 +0900

    ㅇ

---
+++ src/components/MainPageSide.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MainPageSide = () => {
+  return (
+    <>
+      <div className="sideContainer">
+        <div>
+          <h1 className="text-orange flex flex-col justify-center items-center gap-10 m-20">나만의 몽글로드 만들기</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPageSide;

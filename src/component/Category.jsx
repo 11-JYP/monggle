9e6888768e93412c1402080269367cfb3fce88d9
limgi <dntksql1010@naver.com>
2024-09-18 14:04:33 +0900

    add 위치별 재 검색 기능 추가

--- src/component/Category.jsx
+++ src/component/Category.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { CustomOverlayMap, Map, MapMarker } from 'react-kakao-maps-sdk';
+import CategorySearch from './CategorySearch';
 
 const { kakao } = window;
 
@@ -19,31 +20,13 @@ const Category = () => {
     errMsg: null,
     isLoading: true
   });
+  const [map, setMap] = useState(null);
   const [search, setSearch] = useState([]);
-
-  const searchPlaces = (category) => {
-    if (!state.center) return;
-    const ps = new kakao.maps.services.Places();
-
-    const options = {
-      location: new kakao.maps.LatLng(state.center.lat, state.center.lng),
-      radius: 5000,
-      sort: kakao.maps.services.SortBy.DISTANCE
-    };
-    console.log(options);
-
-    ps.keywordSearch(
-      category,
-      (data, status, _pagination) => {
-        if (status === kakao.maps.services.Status.OK) {
-          setSearch(data); // 검색 결과를 search 상태에 저장
-        } else {
-          console.error('검색에 실패하였습니다.');
-        }
-      },
-      options
-    );
-  };
+  const [keyword, setKeyword] = useState('애견카페');
+  const [pagination, setPagination] = useState(null);
+  const [searchId, setSearchId] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lastCenter, setLastCenter] = useState(null);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -75,26 +58,147 @@ const Category = () => {
     }
   }, []);
 
-  console.log(state);
+  const searchPlaces = (center, page) => {
+    if (!state.center) return;
+    const ps = new kakao.maps.services.Places();
+
+    const options = {
+      location: new kakao.maps.LatLng(center.lat, center.lng),
+      radius: 5000,
+      sort: kakao.maps.services.SortBy.DISTANCE,
+      page
+    };
+
+    ps.keywordSearch(
+      keyword,
+      (data, status, pagination) => {
+        if (status === kakao.maps.services.Status.OK) {
+          displayPlaces(data);
+          const bounds = new kakao.maps.LatLngBounds();
+          data.forEach((item) => bounds.extend(new kakao.maps.LatLng(item.y, item.x)));
+          map.setBounds(bounds);
+          setPagination(pagination);
+        } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+          setSearch(data);
+        } else {
+          console.log('검색 실패');
+        }
+      },
+      options
+    );
+  };
+
+  const displayPlaces = (data) => {
+    const bounds = new kakao.maps.LatLngBounds();
+    data.forEach((item) => bounds.extend(new kakao.maps.LatLng(item.y, item.x)));
+    bounds.extend(new kakao.maps.LatLng(state.center.lat, state.center.lng));
+    map.setBounds(bounds);
+    setSearch(data);
+    console.log(data);
+  };
+
+  const moveLatLng = (data) => {
+    const newLatLng = new kakao.maps.LatLng(data.y, data.x);
+    map.panTo(newLatLng);
+  };
+
+  const handleReSearch = () => {
+    if (!map) return;
+    const centerLatLng = map.getCenter();
+    const newCenter = {
+      lat: centerLatLng.getLat(),
+      lng: centerLatLng.getLng()
+    };
+    console.log(centerLatLng);
+    console.log(newCenter);
+    setCurrentPage(1);
+    searchPlaces(newCenter, 1);
+    setLastCenter(newCenter);
+  };
+
+  const handleKeywordSelect = (selectedKeyword) => {
+    setKeyword(selectedKeyword);
+    if (lastCenter) {
+      searchPlaces(lastCenter, 1);
+    } else {
+      searchPlaces(state.center, 1);
+    }
+  };
+
+  useEffect(() => {
+    if (!map) return;
+    setSearchId(null);
+    if (lastCenter) {
+      searchPlaces(lastCenter, currentPage);
+    } else {
+      searchPlaces(state.center, currentPage);
+    }
+  }, [map, keyword, currentPage]);
+
+  useEffect(() => {
+    if (!map) return;
+    const clickListener = () => {
+      setSearchId(null);
+    };
+    kakao.maps.event.addListener(map, 'click', clickListener);
+    return () => {
+      kakao.maps.event.removeListener(map, 'click', clickListener);
+    };
+  }, [map]);
+
   return (
-    <Map center={state.center} style={{ width: '100%', height: 'calc(100vh - 109px)', marginTop: '48px' }} level={3}>
-      <MapMarker position={state.center} />
+    <Map
+      center={state.center}
+      style={{ width: '100%', height: 'calc(100vh - 109px)', marginTop: '48px' }}
+      level={3}
+      onCreate={setMap}
+    >
+      <MapMarker
+        position={state.center}
+        image={{
+          src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
+          size: {
+            width: 50,
+            height: 50
+          }
+        }}
+      />
+      <button onClick={handleReSearch}>현재 위치에서 검색</button>
       {search.map((data) => (
-        <MapMarker
-          key={data.id}
-          position={{ lat: data.y, lng: data.x }}
-          onClick={() => {
-            if (data.id === openMarkerId) {
-              setOpenMarkerId(null);
-            } else {
-              setOpenMarkerId(data.id);
-              moveLatLng(data);
-            }
-          }}
-        />
+        <React.Fragment key={data.id}>
+          <MapMarker
+            key={data.id}
+            position={{ lat: data.y, lng: data.x }}
+            image={{
+              src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
+              size: {
+                width: 35,
+                height: 35
+              }
+            }}
+            onClick={() => {
+              if (data.id === searchId) {
+                setSearchId(null);
+              } else {
+                setSearchId(data.id);
+                moveLatLng(data);
+              }
+            }}
+          />
+          {searchId === data.id && (
+            <CustomOverlayMap yAnchor={2.1} position={{ lat: data.y, lng: data.x }} clickable>
+              <p>{data.place_name}</p>
+              <strong>{data.address_name}</strong>
+              <span>{data.phone}</span>
+              <a href={data.place_url} target="_blank">
+                주소
+              </a>
+            </CustomOverlayMap>
+          )}
+        </React.Fragment>
       ))}
       {KEYWORD_LIST.map((keyword) => (
-        <button key={keyword.id} onClick={() => searchPlaces(keyword.value)}>
+        <button key={keyword.id} onClick={() => handleKeywordSelect(keyword.value)}>
           {keyword.value}
         </button>
       ))}

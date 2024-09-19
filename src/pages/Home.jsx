d282b1fb1abc9a21504d97e3f064da337c8badd0
jungminji <wjdalswl0215@gmail.com>
2024-09-19 14:56:10 +0900

    fix : 충돌 파일 되돌림

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import { useEffect } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
-import Category from '../component/Category';
 
 const Home = () => {
-  return <Category />;
+  return (
+    <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: '600px', height: '500px', borderRadius: '20px' }}>
+      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+        <div style={{ color: '#000' }}>Hello World!</div>
+      </MapMarker>
+    </Map>
+  );
 };
 
 export default Home;

6e94f7cf62ad437ba49cf35113e1026d8ef5a8e2
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-09-20 21:57:18 +0900

    feat: 내 루트 다운받기 추가

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getRouteInfo } from "../api/pathDataSave";
 import CanvasComponent from "../pages/CanvasComponent";
+import html2canvas from "html2canvas";
 
 const Modal = ({ user, onClose }) => {
   const [userRoutes, setUserRoutes] = useState([]); // 사용자 경로 상태
-
+  const refs = useRef([]);
   useEffect(() => {
     const checkUserRoutes = async () => {
       try {
@@ -27,6 +28,21 @@ const Modal = ({ user, onClose }) => {
     checkUserRoutes();
   }, [user.id]); // user.id가 변경될 때마다 실행
 
+  const onClickDownloadButton = (index) => {
+    const target = refs.current[index];
+    if (!target) {
+      return alert("사진 저장에 실패했습니다.");
+    }
+    html2canvas(target).then((canvas) => {
+      const link = document.createElement("a");
+      document.body.appendChild(link);
+      link.href = canvas.toDataURL("image/png");
+      link.download = `${user.nickname}_route_monggle.png`;
+      link.click();
+      document.body.removeChild(link);
+    });
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 ">
       <div className="bg-white p-4 rounded w-[800px] h-[500px] px-10">
@@ -38,16 +54,24 @@ const Modal = ({ user, onClose }) => {
         </div>
         <div className="pt-20 text-[18px]">{user.nickname}님께서 그린 몽글로드입니다.</div>
         <div className="pt-10 overflow-x-scroll overflow-y-hidden">
-          <div className="flex gap-4 whitespace-nowrap">
+          <div className="flex gap-4 whitespace-nowrap ">
             {userRoutes.length > 0 ? (
-              userRoutes.map((route) => (
-                <div key={route.id} className="relative inline-block">
+              userRoutes.map((route, index) => (
+                <div
+                  key={route.id}
+                  className="relative inline-block cursor-pointer"
+                  ref={(el) => (refs.current[index] = el)}
+                  onClick={() => onClickDownloadButton(index)}
+                >
                   <div className="absolute bottom-0 left-0 p-2 bg-white bg-opacity-70 w-full">
                     <h3 className="pt-1">{route.routeName}</h3>
                     <p>{route.description}</p>
                   </div>
+                  <div className="absolute inset-0 bg-white opacity-0 hover:opacity-70 transition-opacity flex justify-center items-center">
+                    <h1 className="text-lg font-bold">내 몽글로드 다운받기</h1>
+                  </div>
                   <div className="bg-[orange]">
-                    <CanvasComponent size={200} coordinates={route.paths} /> {/* 크기를 200px로 설정 */}
+                    <CanvasComponent size={200} coordinates={route.paths} />
                   </div>
                 </div>
               ))
